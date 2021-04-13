8ea20f8c660be24e1cecf1ac6408fb7b9a94a976
gem-png <82475102+gem-png@users.noreply.github.com>
2021-04-13 18:16:29 -0400

    Add files via upload

---
+++ hateemal.py
@@ -0,0 +1,23 @@
+print("SEND AN ABSOLUTELY SCATHING, WORLD BREAKING, INSANE EMAIL TO SOMEONE YOU HAte-inator.")
+print('''
+
+coded by your favorite! it's me gem!
+''')
+sender = input ('in order for this to work, i need you to input your name here: ')
+print('ok, ' + sender + ', got it!')
+reciever = input ('now i need you to put in who will recieve the email: ')
+print('alright, so ' + reciever + '? interesting. they probably suck')
+print('here is your email. do with it what you wish.')
+print('''
+Hello ''' + reciever + ''', 
+
+  Your performance in our office has been on a steady decline since October. I am sorry to deliver this news via e-mail, 
+and not via putting my fist in your face, that you have been fired. We cannot tolerate your out-landish behavior any 
+longer. The final straw was when we watched you march into the office on June 3rd, 2020, with a fish tank full of live 
+shrimp. why. do you think we want this. do you really think that we want live fucking shrimp in our humble Tennessee 
+office.... how did you even get the shrimp?? whatever. i don't care really. also you brought cornbread in. that is a 
+crime against humanity and you know it. this behavior is unforgivable, and also you are weirdly shit at your job??? 
+not judging, it was just an accumulative factor in this decision. kindly you can take your shrimp and shove them up 
+your arse. have a nice day, and come around at 9 am to collect your things.
+
+best regards, ''' + sender)
